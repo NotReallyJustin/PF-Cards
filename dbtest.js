4e7d430a584c6ddc0cc53e338a19226fa98ec940
Justin <60296568+ComradeDiamond@users.noreply.github.com>
2021-08-27 15:27:38 -0400

    Add files via upload

---
+++ dbtest.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+const client = new MongoClient('mongodb+srv://Admin:123454321@default.uwt1u.mongodb.net/Default?retryWrites=true&w=majority', 
+	{useNewUrlParser: true, useUnifiedTopology: true});
+const collection = client.db('Default').collection('Cards');
+
+client.connect().then(() => {
+	let result = collection.find(
+		{'$text': {'$search': 'Russia Cyberattack'}, 'date': '07/21'},
+		{'score': {'$meta': 'textScore'}}
+	).sort({'score': {'$meta': 'textScore'}}).limit(10);
+
+	result.forEach(res => {
+		console.log('res');
+	});
+
+	//console.log('I am async!')
+	//console.log(result.next());
+});
+
+client.close();
